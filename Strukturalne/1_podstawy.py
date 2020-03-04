f9fd7e2afef9c191c6e0a188eb1d1ed62a2aa0f0
Krzysztof Przybysz <kprzybysz2@edu.cdv.pl>
2020-03-04 15:29:29 +0100

    First File

--- Strukturalne/1_podstawy.py
+++ Strukturalne/1_podstawy.py
@@ -1,1 +1,43 @@
 print("CDV")
+print(10)
+
+'''
+Komentarz
+blokowy
+'''
+
+#potęgowanie
+potega=2 ** 10
+print(potega)
+
+#pobieranie danych z klawiatury
+name=input()
+
+#+konkatenacja
+print("Twoje imie: "+name)
+
+surname=input()
+print("Twoje imie: "+name+" nazwisko: "+surname)
+
+'''
+Użytkowanik podaje z klawiatury swój wiek
+wyświetl dane w formacie: Twój wiek: ...lat
+'''
+
+print("Podaj swój wiek ", end="")
+age = input()
+print(type(age))
+print("Twój wiek: ",age," lat")
+
+age1=20
+print(type(age1))
+
+surname="Kowalski"
+firstLetter=surname[0]
+print(firstLetter)
+
+lenght = len(surname)
+
+lastLetter=surname[len(surname)-1]
+print(lastLetter)
+
